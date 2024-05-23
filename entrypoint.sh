8a7189132ad4763684c529a199aea1e353ee3385
arystambek-dimash <arystambekdimash005@gmail.com>
2024-05-24 02:42:26 +0500

    fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ python manage.py collectstatic --noinput
 echo 'Applying migrations...'
 python manage.py migrate
 
-gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn theater.wsgi:application --bind 0.0.0.0:$PORT
